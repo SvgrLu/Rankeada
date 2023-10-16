3b42854d3d279acdf728e720e3e8448d73fef0e7
SvgrLu <lsvaiger@hotmail.com>
2023-10-16 03:04:18 -0300

    pronto

--- rankeada.js
+++ rankeada.js
@@ -1,5 +1,6 @@
-let victorys = 30
-let defeats =  10
+let level = ""
+let victorys = 150
+let defeats =  67
 let saldo = subtract(victorys, defeats)
 
 function subtract(numA, numB){
@@ -7,5 +8,28 @@ function subtract(numA, numB){
     return calculation
 }
 
+switch (true){
+    case saldo <= 10:
+        level = "Ferro"
+        break
+    case saldo >= 11 && saldo <= 20:
+        level = "Bronze"
+        break
+    case saldo >= 21 && saldo <= 50:
+        level = "Prata"
+        break
+    case saldo >= 51 && saldo <= 80:
+        level = "Ouro"
+        break
+    case saldo >= 81 && saldo <= 90:
+        level = "Diamante"
+        break
+    case saldo >= 91 && saldo <= 100:
+        level = "Lendário"
+        break
+    case saldo >= 101:
+        level = "Imortal"
+        break
+}
 
-console.log("O Herói tem de saldo de " + saldo + " está no nível de ")
+console.log("O Herói tem saldo " + saldo + " e está no nível de " + level)

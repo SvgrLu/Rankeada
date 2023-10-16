e0629067274b018d028da4a13a143348ea31e350
SvgrLu <lsvaiger@hotmail.com>
2023-10-16 02:45:04 -0300

    funcao feita

--- rankeada.js
+++ rankeada.js
@@ -1,1 +1,11 @@
-ola
+let victorys = 30
+let defeats =  10
+let saldo = subtract(victorys, defeats)
+
+function subtract(numA, numB){
+    let conta = numA - numB
+    return conta
+}
+
+
+console.log("O Herói tem de saldo de " + saldo + " está no nível de ")
